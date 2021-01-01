8c2051c736b079d024a7ebbedd147b7fa6fad8de
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-01-01 11:29:56 +0530

    Four point one commit

---
+++ index.js
@@ -0,0 +1,64 @@
+/**
+ * Example 1 - Variable declaration
+ */
+//var a;
+//console.log(a);
+
+
+//console.log(b); //Uncaught ReferenceError: b is not defined
+
+//let c;
+
+//console.log(c);
+
+//const d;   //Uncaught SyntaxError: Missing initializer in const declaration
+
+/**
+ * Example 2 - Let for variable dec and assignment
+ */
+//let myNumber = 10;
+
+//console.log(myNumber);
+
+//let myString;
+//console.log(myString);
+//myString = "Hello from string";
+//console.log(myString);
+
+//myString = "Mew Value for myString variable";
+//console.log(myString);
+
+
+/**
+ * Example 3 - var for variable dec and assignment
+ *
+
+var myNumber = 10;
+
+console.log(myNumber);
+
+
+var myString;
+console.log(myString);
+myString = "Hello from string";
+console.log(myString);
+
+myString = "Mew Value for myString variable";
+console.log(myString);
+/
+
+
+/**
+ * Example 2 - const for variable dec and assignment
+ *
+const myBoolean = true;
+console.log(myBoolean);
+
+//myBoolean = false; // Uncaught TypeError: Assignment to constant variable.
+//const myBoolean = false; //Uncaught SyntaxError: Identifier 'myBoolean' has already been declared
+/
+/**
+ * Challenge 1
+ */
+const myObject = {};
+console.log(myObject);
