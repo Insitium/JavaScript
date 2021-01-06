1c073ccc6a34ac326c6fc51f39a1cdccbb6d9bb9
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-01-06 16:10:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * Section 1
+ * Srithmetic operators
+ */
+let a, b;
+a = 1;
+b = 2;
+
+console.log(a+b);
+console.log(a * b);
+console.log(a / b);
+console.log(a - b);
+
+a = "abc";
+b = 5;
+console.log(a+b);
+console.log(a * b); //NaN
+console.log(a / b); //NaN
+console.log(a - b); //NaN
+
+a = "Hello";
+b = "world";
+console.log(a + " "+b);
