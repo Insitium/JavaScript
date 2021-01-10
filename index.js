865d16e7a40e1b760e709ed062510a8e0d372136
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-01-10 10:20:17 +0530

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Array
-const myArray = [1,2,3];
+/**const myArray = [1,2,3];
 console.log(myArray);
 
 const myArray2 = new Array(1,2,3);
@@ -14,4 +14,31 @@ myArray[3] = 43;        //new palce would be created in the array
 console.log(myArray);
 
 myArray[5] = 55;        //new place would be created at 5th position and 4th positin will be names empty
+console.log(myArray);
+*/
+//Array methods
+const myArray = [1,2,3];
+console.log(myArray);
+
+myArray.push(4);
+myArray.push(true);
+console.log(myArray);      // [1, 2, 3, 4, true]
+
+
+//pop
+myArray.pop();
+
+const removedElement = myArray.pop();
+console.log(myArray);
+console.log(removedElement);
+
+//shift
+myArray.shift();
+const shiftRemovedElement = myArray.shift();
+console.log(myArray);
+console.log(shiftRemovedElement);
+
+//unshift
+myArray.unshift(2);
+myArray.unshift(1);
 console.log(myArray);
