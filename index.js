ea990856e078eaf89122df994ef492442c13247d
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-01-13 13:50:02 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,190 +1,72 @@
 /**
- * Exapmle 1
+ * Challenge 1
  */
-/*for(let i = 0; i<5 ; i++){
-    console.log(i);
-}*/
 /**
- * Exapmle 2
+ * task 1
  */
-/*const myArray = [0, true, "abc", null];
-for(let i = 0; i<myArray.length; i++){
-    console.log(myArray[i]);
-}*/
-//but we can only use dot notation if the peoperty is defined explicitly
-/*const myArray = [0, true, "abc", null];
-myArray.i = "Hello, this is one more proeprty";
-for(let i = 0; i< myArray.length; i++)
+/*const myObject = {
+    key1: true,
+    key5: 10,
+    key3:"abc",
+    key4: null,
+    key10: NaN
+};
+for (let key in myObject)
 {
-    console.log(myArray.i)
-}
-console.log(myArray);*/
-
-/**
- * Exapmle 3
- */
-/*let i = 0;
-while(i >-5){
-    console.log(i);
-    i--;
+    if(key === "key1" || key==="key3")
+    {
+        console.log(myObject[key]);
+    }
 }*/
 
-// wehn we want one action to be performed atleast once then we use do while loop
-/*let i = 100;
+/**
+ * task 2
+ * generatenew 4 digit random number
+ * ensure taht this new random number does not match any existing number of the array
+ * range is between 1000 and 9999
+ */
+const myNumbers = [
+    2355,
+    1342,
+    3546,
+    5423,
+    6425,
+    3566
+];
+let newRandomNumber;
+let check = 0;
+/*while(true){
+    newRandomNumber = Math.floor((Math.random()+1) * 1000);
+    for(let i = 0; i<myNumbers.length; i++){
+        if(newRandomNumber !== myNumbers[i]){
+            check = 1;
+            break;
+        }
+        
+    }
+    if(check = 1){
+        break;
+    }
+}
+console.log(newRandomNumber);*/
+
+/*//using functions
+function getRandomInt(){
+    return Math.floor((Math.random()+1)*1000);
+}
+function isNotUnique(newRandomNumber){
+    //compare num with numbers in the MyNumbers array
+    for(let number of myNumbers){
+        if(number === newRandomNumber){
+            console.log(newRandomNumber+" is not unique"); 
+            return true;
+        }
+    }
+    //ti will return undefined here if we wont add return false which in turn would work teh same for while loop
+}
 do{
-    console.log(i);
-    i++;
-}while(i<10);*/
-
-/**
- * Example 5
- * for loops with objects
- */
-/*const myObjects = {
-    a: "abc",
-    b:true,
-    c:null,
-    d:150
-};
-for(let key in myObjects){
-    console.log(key);
-    console.log(myObjects[key]);
-    console.log("---------");
-}*/
-
-/**
- * Example 6
- * for loops with objects with imherited objects
- */
-/*const myObjects = {
-    a: "abc",
-    b:true,
-    c:null,
-    d:150
-};
-Object.prototype.globalProp = "Inherited property";     //adds a property to the object like we have proto
-for(let key in myObjects){
-    console.log(key, myObjects[key]);
-}*/
-
-/**
- * Example 7
- * for in loop with arrays
- */
-/*const myArray = [true, {}, {} ,10];
-Array.prototype.globalProp = "Global array property";
-for(let key in myArray){
-    console.log(key , myArray[key])
-}*/
-
-/**
- * Example 8 
- * for of loop with arrays
- */
-/*const persons = [{
-    name: "Mike",
-    age: 25
-},
-{chrome
-    name:"bob",
-    age:30
-},{name:"Alice", age:27}
-]
-for(let person of persons){
-    console.log(person.name +  " is "+person.age + " years old");
-}*/
-
-
-/**
- * Switch statements
- */
-/*switch(Expression){
-    case A:
-        // Action if expression == A
-        break;
-    case B:
-        //actions if Expression == B
-        break;
-    default:
-        //default actions
-}*/
-/**
- * Example
- */
-/*let month = 2;
-switch (month){
-    case 1:
-        console.log("It is december");
-        break;
-    case 2:
-        console.log("January");
-        break;
-    case 3:
-        console.log("february");
-        break;
-    default:
-        console.log("Not a winter month");
-}*/
-
-/**
- * Ternary operators
- */
-//condition ? Expression1 : Expression2
-/**
- * or
-condition
-    ? Expression1
-    : Expression2
- */
-/*let value = 11;
-value
-? console.log("Value is true")
-: console.log("Value is false");
-*/
-/*let value = 11;
-console.log(value>0 ? value : -value);*/
-
-
-/**
- * Switch statement example
- */
-/*let color = "green";
-if(color === "green"){
-    console.log("It is green color");
-}else if (color === "Yellow"){
-    console.log("Yellow color");
-}else if( color === "red"){
-    console.log("red color");
-}else{
-    console.log("Color is unknown");
-}
-
-switch(color){
-    case "green":
-        console.log("green color");
-        break;
-    case "yellow":
-        console.log("yellow color");
-        break;
-    default:
-        console.log("colorless");
-        break;
-}*/
-
-
-/**
- * Ternary operator example
- */
-/*let i = 10;
-let j;
-
-j = i < 100 ? i : 0;
-console.log(j);
-
-i >=10 && j
-? console.log("true")
-: console.log("false");
-
-console.log(i);
-console.log(j);*/
+    newRandomNumber = getRandomInt();
+}while(isNotUnique(newRandomNumber));
+myNumbers.push(newRandomNumber);
+console.log(myNumbers);*/
 
