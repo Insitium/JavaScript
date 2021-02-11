f94ff3d4e49c854781e1ff9503c927e9e1f93113
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-02-11 10:12:26 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,44 +1,66 @@
-//eXAMPLE 1
-// call method
-/**const myObject = {
-    a:10,
+/**
+ * call - custom this
+ */
+/*const myObject = {
+    a: 10,
     b: null
 };
 function myFunction(){
     console.log(this);
 }
+
 myFunction();
-myFunction(myObject);
-myFunction.call(myObject); //this is myObject
-*/
 
-//EXAMPLE 2
-/*console.log(this);  //window
-console.log(this === window);   //true
-this.console.log("Method of this");
+myFunction.call(myObject);*/
 
-
-//Example 3 -- 'this inside a function'
-//in strict mode
-"use strict";
-function myFunction(){
+//Example 2
+/*const myObject = {
+    a: 10,
+    b: null
+};
+function myFunction(a,b){
+    console.log(a+b);
     console.log(this);
 }
 
 myFunction();
 
-window.myFunction();
-*/
+myFunction.call(myObject, 10,3);*/
 
-
-//example 4 -- this in side of the methods of the object
-/*const myObject = {
-    name: "Alice",
-    age: 20,
-    greeting: function(){
-        console.log(this);
-        console.log("Name of the person is "+this.name+ " and the age is "+this.age);  //this is equal to {name: "Alice", age:20, greeting:..}
+//Exapmle 3 - Call method of the object with other object as "this"
+/*const person1 = {
+    city: "ny",
+    name: "bob",
+    info: function(){
+        console.log(this.name+" lives in "+this.city);
     }
 };
-myObject.greeting();
+
+const person2 = {
+    city: "Paris",
+    name: "Alice"
+};
+person1.info(); //bob lives in ny
+
+person1.info.call(person2); //Alice lives in Paris
 */
+
+/**
+ * Example 4 -Apply method an custon "this" arguments
+ */
+const myObject = {
+    a: 10,
+    b: null
+};
+function myFunction(a,b,c){
+    let sum = a+b+c;
+    //console.log(this);
+    for (let key in this){
+        if(typeof this[key] == "number"){
+            sum+= this[key];
+        }
+    }
+    console.log(sum);
+}
+
+myFunction.apply(myObject,[10, 3, 5]);
